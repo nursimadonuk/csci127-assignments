b1f57478c1267df6e3bafee0ed91bf49e4015962
Nursima <nursima.nursima.com>
2018-09-06 09:19:41 -0400

    greeter and abba

---
+++ lab_01/greeter.py
@@ -0,0 +1,18 @@
+def greeter (name) :
+    return "hello " + name + "!"
+print (greeter("stan"))
+#once per machine ; git clone
+#once per file ; git add
+
+#sit down
+# cd into folder
+#git pull
+
+#work on program
+#git add any new file once
+#git commit -a -m "..."
+#git push
+
+def make_abba(a, b):
+  return a + b + b + a
+print (make_abba("hello","world"))

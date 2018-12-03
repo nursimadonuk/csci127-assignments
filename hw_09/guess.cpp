63b5944c31364de172d74324a7ced1a1e89a9c38
nursima <nursima.nursima.com>
2018-12-03 08:11:44 -0500

    i couldnt do the guess one

--- hw_09/guess.cpp
+++ hw_09/guess.cpp
@@ -1,7 +1,25 @@
 #include<iostream>
+#include<random>
+
 
 int main(){
  int i;
  std::cin>> i;
+ 
  std::cout<<i<<"\n";
+}
+
+int guess(){
+ int a 
+ a = rand() %100;
+ std::cout<<a;
+ int b
+ std::cin>> b;
+ if (b = 0)
+  break
+ else if (b = 1)
+  a = rand() % a + 100
+ else if (b= -1)
+ 
+
 }

bad9f6187cfc53b30e0716dbe6f41ad45a3b52d0
nursima <nursima.nursima.com>
2018-12-05 18:45:32 -0500

    fixed guess.cpp

--- hw_09/guess.cpp
+++ hw_09/guess.cpp
@@ -1,25 +1,39 @@
 #include<iostream>
 #include<random>
+#include<cstdlib>
+#include<ctime>
 
 
 int main(){
- int i;
- std::cin>> i;
- 
- std::cout<<i<<"\n";
+ //establish the computers number
+ srand(time(NULL));
+ int answer = rand() %100;
+ std::cout << answer << "\n";
+
+ // repeat until the answer is guessed
+ int guess;
+ std::cout<<"Please enter a guess"<<"\n";
+ std::cin >> guess;
+ int guesses = 1;
+ while (guess != answer){
+  if (guess > answer){
+   std::cout<< "You guessed too high\n";
+  }else if (guess < answer){
+    std::cout<< "You guessed too low \n";
+  }
+ std::cout<<"Pleas enter a guess"<<"\n";
+ std::cin>> guess;
+ guesses++;
+ }
+
+ std::cout<<"Congragulations you have guessed the number!!";
+ std::cout<<"It took you "<< guesses << " guesses.\n";
+ //get a guess from the user
+ //see if the guess is too low/high just right
+
+  
+ return 0;
 }
 
-int guess(){
- int a 
- a = rand() %100;
- std::cout<<a;
- int b
- std::cin>> b;
- if (b = 0)
-  break
- else if (b = 1)
-  a = rand() % a + 100
- else if (b= -1)
- 
 
-}
+ 
